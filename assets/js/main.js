5b9c80042f59d660528c0f7cac7468770d9fabf8
Stef Cruz <crzstefanie@gmail.com>
2020-09-03 07:43:01 +0100

    css: add bootstrap grid to the card recipes

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ const searchButton = document.querySelector("#search");
 const recipeCards = document.querySelector("#content");
 const errorHandling = document.querySelector("#error-handling");
 
+
 // ----------------------------------------------- Event Listeners
 
 searchButton.addEventListener("click", () => {
@@ -17,6 +18,15 @@ searchButton.addEventListener("click", () => {
 
 let appId = 'ba5b7a21',
     apiKey = 'd1d3afcdc37dd030c29294267aaedbc8';
+
+
+// Function to get value from checked checkbox
+
+    function check() {
+        if (document.getElementsByName("health-labels").checked = true) {
+            let checkBoxHealth
+        };
+    }
 
 // Function to get data from Edamam Recipe API
 
@@ -37,19 +47,26 @@ async function searchRecipe() {
 // Using the data
 
 function useApiData(data) {
-    let cardImage = data.hits[0].recipe.image
-    let cardTitle = data.hits[0].recipe.label
-    let aLink = data.hits[0].recipe.url
-
-    recipeCards.innerHTML = `
-    <div class="card" style="width: 18rem;">
-        <img src="${cardImage}" class="card-img-top" alt="Recipe image">
-            <div class="card-body">
-                <h5 class="card-title">${cardTitle}</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="${aLink}" target="_blank" class="btn btn-primary">See Recipe</a>
+    for (let i = 0; i < 8; i++) {
+        recipeCards.innerHTML += `
+    <div class="col-12 col-md-6 col-xl-3 col-lg-3">
+        <div class="card">
+        <img src="${data.hits[i].recipe.image}"
+            class="card-img-top" alt="Recipe image">
+        <div class="card-body">
+            <h5 class="card-title" id="card-title">${data.hits[i].recipe.label}</h5>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Calories: ${parseInt(data.hits[i].recipe.calories)}</li>
+            <li class="list-group-item">Ingredients used: ${data.hits[i].recipe.ingredients.length}</li>
+            <li class="list-group-item">Health labels: ${data.hits[i].recipe.healthLabels}
+            </li>
+        </ul>
+        <a href="${data.hits[i].recipe.url}" target="_blank" class="btn btn-primary">See Recipe</a>
         </div>
     </div>
-    `
+        `
+    }
+
 }
 
