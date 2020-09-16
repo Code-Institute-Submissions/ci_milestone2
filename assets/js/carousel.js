0a49671bea58e7106114aec0806fc6e3316b3a4e
Stef Cruz <crzstefanie@gmail.com>
2020-09-16 22:35:10 +0100

    Bug: JS/CSS solution for recipe cards in the carousel to display on row

---
+++ assets/js/carousel.js
@@ -0,0 +1,24 @@
+// Animation carousel taken from https://www.codeply.com/go/UBE4KZIeSJ
+
+// $('#myCarousel').carousel({
+//     interval: 10000
+//   })
+  
+  $('.carousel .carousel-item').each(function(){
+      var minPerSlide = 3;
+      var next = $(this).next();
+      if (!next.length) {
+      next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+      
+      for (var i=0;i<minPerSlide;i++) {
+          next=next.next();
+          if (!next.length) {
+              next = $(this).siblings(':first');
+            }
+          
+          next.children(':first-child').clone().appendTo($(this));
+        }
+  });
+  
