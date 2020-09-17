ab6aa527dcc7d9d181c72d0876b5cf46616f4e03
Stef Cruz <crzstefanie@gmail.com>
2020-09-17 21:08:25 +0100

    Bug: Fixed carousel animation

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,19 +1,22 @@
 // Animation carousel taken from https://www.codeply.com/go/UBE4KZIeSJ, original from https://stackoverflow.com/questions/20007610/bootstrap-carousel-multiple-frames-at-once
 
-// $('#myCarousel').carousel({
+// $('#carousel').carousel({
 //     interval: 10000
 //   })
   
   $('.carousel .carousel-item').each(function(){
       var minPerSlide = 3;
       var next = $(this).next();
+
       if (!next.length) {
       next = $(this).siblings(':first');
       }
+
       next.children(':first-child').clone().appendTo($(this));
       
       for (var i=0;i<minPerSlide;i++) {
           next=next.next();
+
           if (!next.length) {
               next = $(this).siblings(':first');
             }
@@ -21,4 +24,5 @@
           next.children(':first-child').clone().appendTo($(this));
         }
   });
+  
   
