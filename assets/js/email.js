416aecf73d7d953dbcfff417f8d8616e006311de
Stef Cruz <crzstefanie@gmail.com>
2020-10-01 20:58:58 +0100

    Add data validation to contact form

--- assets/js/email.js
+++ assets/js/email.js
@@ -2,14 +2,41 @@
     emailjs.init("user_Oay0ciCVLZ3t84nUMqX94");
 })();
 
-window.onload = function () {
-    document.getElementById('contact-form').addEventListener('submit', function (event) {
-        event.preventDefault();
-        emailjs.sendForm('gmail', 'milestone2', '#contact-form')
-            .then(function (response) {
-                console.log('SUCCESS!', response.status, response.text);
-            }, function (error) {
-                console.log('FAILED...', error);
-            });
-    });
-}
+// window.onload = function () {
+//     document.getElementById('contact-form').addEventListener('submit', function (event) {
+//         event.preventDefault();
+//         emailjs.sendForm('gmail', 'milestone2', '#contact-form')
+//             .then(function (response) {
+//                 console.log('SUCCESS!', response.status, response.text);
+//             }, function (error) {
+//                 console.log('FAILED...', error);
+//             });
+//     });
+// }
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          } else { //send email with emailJS
+            event.preventDefault();
+            emailjs.sendForm('gmail', 'milestone2', '#contact-form')
+                .then(function (response) {
+                    console.log('SUCCESS!', response.status, response.text);
+                }, function (error) {
+                    console.log('FAILED...', error);
+                });
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
