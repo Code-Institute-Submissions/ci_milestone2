d5180b17d83dfa2a41d92ef8a7c882fa6c500336
Stef Cruz <crzstefanie@gmail.com>
2020-10-01 07:53:47 +0100

    Fix: Email form was being sent when onload. Now sending when submit is clicked

--- assets/js/email.js
+++ assets/js/email.js
@@ -2,9 +2,14 @@
     emailjs.init("user_Oay0ciCVLZ3t84nUMqX94");
 })();
 
-emailjs.sendForm('gmail', 'milestone2', '#contact-form')
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
+window.onload = function () {
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        emailjs.sendForm('gmail', 'milestone2', '#contact-form')
+            .then(function (response) {
+                console.log('SUCCESS!', response.status, response.text);
+            }, function (error) {
+                console.log('FAILED...', error);
+            });
     });
+}
