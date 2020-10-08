2a45b8a7bc0a11f03aa5ef884bd9b3f6075643dd
Stef Cruz <crzstefanie@gmail.com>
2020-10-08 21:52:22 +0100

    Add positive/negative feedback for when form is submitted

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,37 +1,34 @@
+//Selectors
+let messageNotSent = document.getElementById("message-not-sent-container");
+let messageSent = document.getElementById("message-sent-container");
+let forms = document.getElementsByClassName('needs-validation');
+
+//Initialise emailJS
 (function () {
     emailjs.init("user_Oay0ciCVLZ3t84nUMqX94");
 })();
 
-// window.onload = function () {
-//     document.getElementById('contact-form').addEventListener('submit', function (event) {
-//         event.preventDefault();
-//         emailjs.sendForm('gmail', 'milestone2', '#contact-form')
-//             .then(function (response) {
-//                 console.log('SUCCESS!', response.status, response.text);
-//             }, function (error) {
-//                 console.log('FAILED...', error);
-//             });
-//     });
-// }
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields. Source: Bootstrap.
+//Bootstrap form validation
 (function() {
     'use strict';
     window.addEventListener('load', function() {
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-      var forms = document.getElementsByClassName('needs-validation');
       // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function(form) {
+      let validation = Array.prototype.filter.call(forms, function(form) {
         form.addEventListener('submit', function(event) {
           if (form.checkValidity() === false) {
             event.preventDefault();
             event.stopPropagation();
-            // Hide modal, this option doesn't work: document.getElementById("submit-modal").style.display = "block";
-          } else { //send email with emailJS
+            messageNotSent.style.display = "block";
+          } else { 
+            //send email with emailJS
             event.preventDefault();
             emailjs.sendForm('gmail', 'milestone2', '#contact-form')
                 .then(function (response) {
                     console.log('SUCCESS!', response.status, response.text);
+                    if (messageNotSent.style.display = "block") {
+                      messageNotSent.style.display = "none";
+                      messageSent.style.display = "block";
+                    }
                 }, function (error) {
                     console.log('FAILED...', error);
                 });
